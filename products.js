35487f0a6135c4b206c0a61006fdb05d481915c7
MihaiVatavu <66440300+MihaiVatavu@users.noreply.github.com>
2021-01-05 14:36:53 +0000

    Finished the home and products page(html, styling and the jquery on adding classes on the mobile version)

---
+++ products.js
@@ -0,0 +1,9 @@
+$(window).on('resize', function () {
+  if ($(window).width() < 1000) {
+    $('#imgtwo').addClass('order-last');
+    $('#imgfour').addClass('order-last');
+  } else {
+    $('#imgtwo').removeClass('order-last');
+    $('#imgfour').removeClass('order-last');
+  }
+});
